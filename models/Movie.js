9b2b70948708227b8793d189f10064498e4f6990
Charlie-robin <charlie.robin.richardson@gmail.com>
2025-01-27 17:28:48 +0000

    Completed Workshop 1

--- models/Movie.js
+++ models/Movie.js
@@ -1,53 +1,56 @@
-import db from '../db/index.js'
+import db from "../db/index.js";
 
 class Movie {
   static allowedGenres = [
-    'Adventure',
-    'Action',
-    'Animation',
-    'Biography',
-    'Crime',
-    'Drama',
-    'Fantasy',
-    'History',
-    'Horror',
-    'Mystery',
-    'Romance',
-    'War'
-  ]
+    "Adventure",
+    "Action",
+    "Animation",
+    "Biography",
+    "Crime",
+    "Drama",
+    "Fantasy",
+    "History",
+    "Horror",
+    "Mystery",
+    "Romance",
+    "War",
+  ];
 
   static async findAll(genre) {
     const query = [
-      'select movies.*',
-      'from movies',
-      'left join reviews on movies.id = reviews.movieId'
-    ]
+      "select movies.*",
+      "from movies",
+      "left join reviews on movies.id = reviews.movieId",
+    ];
 
-    const values = []
+    const values = [];
 
     if (genre) {
-      query.push('where lower(movies.genre) like ?')
-      values.push('%' + genre + '%')
+      query.push("where lower(movies.genre) like ?");
+      values.push("%" + genre + "%");
     }
 
-    query.push('group by movies.id')
-    query.push('order by movies.releaseDate desc')
+    query.push("group by movies.id");
+    query.push("order by movies.releaseDate desc;");
 
-    const results = await db.raw(query.join(' '), values)
-    return results
+
+    const results = await db.raw(query.join(" "), values);
+
+
+    return results;
   }
 
   static async findById(id) {
-    const query = 'select * from movies where id = ?'
-    const results = await db.raw(query, [id])
-    return results[0]
+    const query = "select * from movies where id = ?";
+    const results = await db.raw(query, [id]);
+    return results[0];
   }
 
   static async findReviews(id) {
-    const query = 'select * from reviews where movieId = ?'
-    const results = await db.raw(query, [id])
-    return results
+    const query = "select * from reviews where movieId = ?";
+    const results = await db.raw(query, [id]);
+    return results;
   }
 }
 
-export default Movie
+export default Movie;
--- server/app.js
+++ server/app.js
@@ -1,32 +1,34 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+import Movie from "../models/Movie.js";
+
+const app = express();
 
 // TODO: add your endpoints here
-app.use(express.static("exercises/public"));
+app.use(express.static("public"));
 
-app.set("views", "exercises/views"); 
+app.set("views", "views");
 
 app.set("view engine", "ejs");
 
-app.get("/d1e2", (req, res) => {
-    const msg = "Hello from d1e2!";
-    // TODO: Render 'd1e2.ejs' and pass the msg
-    res.render("d1e2.ejs", { msg });
-  });
+app.get("/", async (req, res) => {
+  const movies = await Movie.findAll(req.query.genre);
 
-  app.get("/d1e3", (req, res) => {
-    const shopping = [
-      "Eggs",
-      "Flour",
-      "Sugar",
-      "Lifesize cutout of Christian Bale as Batman",
-      "Milk",
-      "Bread",
-    ];
-  
-    // TODO: Render 'd1e3.ejs', passing the value of `shopping
-    res.render("d1e3.ejs", { shopping });
+  res.render("index", {
+    genres: Movie.allowedGenres,
+    query: req.query,
+    movies,
   });
+});
 
-export default app
+app.get("/movie/:movieId", async (req, res) => {
+  const { movieId } = req.params;
+  const movie = await Movie.findById(movieId);
+  const reviews = await Movie.findReviews(movieId);
+  res.render("movie/index", {
+    movie,
+    reviews,
+  });
+});
+
+export default app;
